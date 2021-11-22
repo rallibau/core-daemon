3dcedf3f0599c059e00f645b6cab2eff8ca827ef
rallibau <rallibau@gmail.com>
2021-11-22 17:14:03 +0100

    refactor test and read file

--- README.md
+++ README.md
@@ -334,3 +334,6 @@ GNU Affero General Public License for more details.
 
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see http://www.gnu.org/licenses/.
+
+
+node 10.24 - yarn --ignore-engines
--- test/fileStorage.unit.js
+++ test/fileStorage.unit.js
@@ -3,36 +3,96 @@ const FileStorageAdapter = require('../lib/customStorage/adapters/file');
 const storj = require('storj-lib');
 const {expect} = require("chai");
 
+function getRandomString(length) {
+    let s = '';
+    do {
+        s += Math.random().toString(36).substr(2);
+    } while (s.length < length);
+    s = s.substr(0, length);
 
-describe('class:FileStorageAdapter', function () {
-    const item = storj.StorageItem({
-        hash: storj.utils.ripemd160('shardhash'),
+    return s;
+}
+
+const itemMother = function () {
+    const uid = getRandomString(32);
+    return storj.StorageItem({
+        hash: storj.utils.ripemd160(uid),
         contracts: {
             node3: {
-                data_hash: storj.utils.ripemd160('shardhash')
+                data_hash: storj.utils.ripemd160(uid)
             }
         }
     });
+};
 
+const cleanItem = function (storageManager, hash, callback) {
+    storageManager._del(hash, function (error) {
+        console.log(error)
+        if (!error) {
+            callback();
+        } else {
+            callback(error);
+        }
+    });
+};
+
+const createItem = function (storageManager, hash, object, callback) {
+    storageManager._put(hash, object, function (error) {
+        if (!error) {
+            callback();
+        } else {
+            callback(error.error);
+        }
+    });
+};
+
+describe('class:FileStorageAdapter', function () {
     let storageManager = new FileStorageAdapter('path/to/store/');
     describe('#validate store and get', function () {
         it('should store item', function (done) {
-            let rpc = storageManager._put(item.hash, item.toObject(), function (error) {
-                if(!error){
-                    done();
+            const item = itemMother();
+            createItem(storageManager, item.hash, item.toObject(), function (error) {
+                if (error) {
+                    done(error.error);
                 }
-            })
-
-        });
-        it('should get item', function (done) {
-            storageManager._get(item.hash,function (error,result){
-                if(!error){
-                    done();
-                }
+                done();
+                cleanItem(storageManager, item.hash);
             });
 
         });
+        it('should get item', function (done) {
+            const item = itemMother();
+            createItem(storageManager, item.hash, item.toObject(), function (error) {
+                if (error) {
+                    done(error.error);
+                }
+                storageManager._get(item.hash, function (error) {
+                    if (error) {
+                        done(error.error);
+                    }
+                    done();
+                    cleanItem(storageManager, item.hash);
+                });
+            });
 
+
+        });
+        it('should delete item', function (done) {
+            const item = itemMother();
+            createItem(storageManager, item.hash, item.toObject(), function (error) {
+                if (error) {
+                    done(error.error);
+                }
+                cleanItem(storageManager, item.hash, function (error) {
+                    if (error) {
+                        done(error.error);
+                    }
+                    done();
+                });
+            });
+
+
+        });
     });
 
 });
