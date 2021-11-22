b0e1e0e71c239a37e7f64f39b7b47bdd11ddfa1a
rallibau <rallibau@gmail.com>
2021-11-22 11:04:12 +0100

    test store and load

--- test/fileStorage.unit.js
+++ test/fileStorage.unit.js
@@ -5,27 +5,31 @@ const {expect} = require("chai");
 
 
 describe('class:FileStorageAdapter', function () {
-    let storageManager = new FileStorageAdapter('path/to/store');
+    const item = storj.StorageItem({
+        hash: storj.utils.ripemd160('shardhash'),
+        contracts: {
+            node3: {
+                data_hash: storj.utils.ripemd160('shardhash')
+            }
+        }
+    });
 
-    describe('#validatePath', function () {
-        it('should callback error if no share', function (done) {
+    let storageManager = new FileStorageAdapter('path/to/store/');
+    describe('#validate store and get', function () {
+        it('should store item', function (done) {
+            let rpc = storageManager._put(item.hash, item.toObject(), function (error) {
+                if(!error){
+                    done();
+                }
+            })
 
-            const item = storj.StorageItem({
-                hash: storj.utils.ripemd160('shardhash'),
-                contracts: {
-                    node3: {
-                        data_hash: storj.utils.ripemd160('shardhash')
-                    }
+        });
+        it('should get item', function (done) {
+            storageManager._get(item.hash,function (error,result){
+                if(!error){
+                    done();
                 }
             });
-
-
-            let rpc = storageManager._put(item.hash, item.toObject(), function (error) {
-                if(error){
-                    console.log(error);
-                }
-                done();
-            })
 
         });
 
